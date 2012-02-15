89e3669ac8d76187f51cfed069d86fd78e3bb424
Kevin McMahon <kmcmahon@redpointtech.com>
2012-02-15 13:07:09 -0600

    added adb tab complete

--- bash/exports.bash
+++ bash/exports.bash
@@ -18,10 +18,10 @@ export HISTIGNORE="ls:ls *:cd:cd -:pwd;exit:date:* --help"
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US"
 
+export M2_HOME=/usr/share/maven
 export ANDROID_HOME=/Developer/android
 export GRADLE_HOME=~/Tools/gradle-1.0-milestone-7
-export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:~/Tools:$GRADLE_HOME/bin:~/Tools/dex2jar
+export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:~/Tools:~/Tools/bin:$GRADLE_HOME/bin:~/Tools/dex2jar:$M2_HOME
 export DATABASE_URL=postgres://kmcmahon:0c791fd488@beta.spacialdb.com:9999/spacialdb_1321928742fe_kmcmahon
 
 export PS1='\h:\W$(__git_ps1 "[\[\e[0;32m\]%s\[\e[0m\]\[\e[0;33m\]$(parse_git_dirty)\[\e[0m\]]")$ '
-export M2_HOME=/usr/share/maven

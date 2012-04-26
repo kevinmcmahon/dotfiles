f8dc96aa838ecfd22c8d6e5fdcbf44707b37ab2d
Kevin McMahon <kmcmahon@redpointtech.com>
2012-04-25 20:09:13 -0500

    add some heroku and osx aliases

---
+++ bash/osx.aliases.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Desktop Programs
+alias preview="open -a '$PREVIEW'"
+alias xcode="open -a '/Developer/Applications/Xcode.app'"
+alias filemerge="open -a '/Developer/Applications/Utilities/FileMerge.app'"
+alias chrome="open -a google\ chrome"
+alias f='open -a Finder '

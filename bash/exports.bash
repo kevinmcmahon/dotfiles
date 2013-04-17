ed44c02be24d30d3414ea7c30e29b92129f36792
Kevin McMahon <klmcmahon+github@gmail.com>
2013-04-17 14:28:18 -0500

    updates

--- bash/exports.bash
+++ bash/exports.bash
@@ -25,5 +25,10 @@ export M2_HOME=/usr/share/maven
 export ANDROID_HOME=~/developer/android
 export GRADLE_HOME=~/Tools/gradle-1.0-rc-3
 export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:~/Tools:~/Tools/bin:$GRADLE_HOME/bin:~/Tools/dex2jar:$M2_HOME
+#export DATABASE_URL=postgres://kmcmahon:0c791fd488@beta.spacialdb.com:9999/spacialdb_1321928742fe_kmcmahon
+#export DATABASE_URL=postgres://mnaijy_wasnok:63992490@spacialdb.com:9999/mnaijy_wasnok
+#export DATABASE_URL=postgres://qwjvxk_fbrczr:da4ed395@spacialdb.com:9999/qwjvxk_fbrczr
 
 export PS1='\[\033[G\]\h:\W$(__git_ps1 "[\[\e[0;32m\]%s\[\e[0m\]\[\e[0;33m\]$(parse_git_dirty)\[\e[0m\]]")$ '
+
+export PROMPT_COMMAND='echo -ne "\033]0; ${PWD##*/}\007"'

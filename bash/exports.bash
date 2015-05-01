9ae0af2977d91d534a82c5925a1d85017be03626
Kevin McMahon <klmcmahon@gmail.com>
2015-05-01 12:06:44 -0500

    update exports

--- bash/exports.bash
+++ bash/exports.bash
@@ -1,6 +1,3 @@
-export M2_HOME=/usr/local/Cellar/maven/3.1.1/libexec
-export ANDROID_HOME=~/tools/android-sdk
-
 if [ -f ~/dotfiles/bash/exports.bash.local ]; then
 	source ~/dotfiles/bash/exports.bash.local	
 else
@@ -32,4 +29,3 @@ export LANG="en_US"
 export PS1='\[\033[G\]\h:\W$(__git_ps1 "[\[\e[0;32m\]%s\[\e[0m\]\[\e[0;33m\]$(parse_git_dirty)\[\e[0m\]]")$ '
 
 export PROMPT_COMMAND='echo -ne "\033]0; ${PWD##*/}\007"'
-export JAVA_HOME=`/usr/libexec/java_home` 

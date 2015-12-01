b62c79993de0c8309a2ed3d00120d05788c77cdf
Kevin McMahon <kevinmcmahon@users.noreply.github.com>
2015-12-01 12:02:45 -0600

    split functions out to osx and linux

--- bash/functions.bash
+++ bash/functions.bash
@@ -6,60 +6,8 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
 }
 
-function copy {
-	if [[ $1 =~ ^-?[hH] ]]; then
- 
-		echo "Intelligently copies command results, text file, or raw text to"
-		echo "OS X clipboard"
-		echo
-		echo "Usage: copy [command or text]"
-		echo "  or pipe a command: [command] | copy"
-		return
-	fi
- 
-	local output
-	local res=false
-	local tmpfile="${TMPDIR}/copy.$RANDOM.txt"
-	local msg=""
- 
-	if [[ $# == 0 ]]; then
-		output=$(cat)
-		msg="Input copied to clipboard"
-		res=true
-	else
-		local cmd=""
-		for arg in $@; do
-			cmd+="\"$(echo -en $arg|sed -E 's/"/\\"/g')\" "
-		done
-		output=$(eval "$cmd" 2> /dev/null)
-		if [[ $? == 0 ]]; then
-			msg="Results of command are in the clipboard"
-			res=true
-		else
-			if [[ -f $1 ]]; then
-				output=""
-				for arg in $@; do
-					if [[ -f $arg ]]; then
-						type=`file "$arg"|grep -c text`
-						if [ $type -gt 0 ]; then
-							output+=$(cat $arg)
-							msg+="Contents of $arg are in the clipboard.\n"
-							res=true
-						else
-							msg+="File \"$arg\" is not plain text.\n"
-						fi
-					fi
-				done
-			else
-				output=$@
-				msg="Text copied to clipboard"
-				res=true
-			fi
-		fi
-	fi
- 
-	$res && echo -ne "$output" | pbcopy -Prefer txt
-	echo -e "$msg"
-}
-
-function anybar { echo -n $1 | nc -4u -w0 localhost ${2:-1738}; }
+if [ "$(uname)" == "Darwin" ]; then
+	source ~/dotfiles/bash/functions.bash.osx
+else
+	source ~/dotfiles/bash/functions.bash.linux
+fi

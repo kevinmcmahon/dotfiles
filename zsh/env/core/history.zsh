b4117f992a75e021a3fe19b2bbb852216e3ef6f0
Kevin McMahon <kevinmcmahon@users.noreply.github.com>
2025-02-22 10:12:06 -0600

    clean up some zsh path handling

--- zsh/env/core/history.zsh
+++ zsh/env/core/history.zsh
@@ -1,6 +1,6 @@
 # Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
-HISTSIZE=1000
-SAVEHIST=1000
+HISTSIZE=100000
+SAVEHIST=100000
 HISTFILE=~/.zsh_history
 setopt HIST_IGNORE_DUPS
 setopt HIST_IGNORE_SPACE
---
+++ zsh/env/core/path.zsh
@@ -0,0 +1,1 @@
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH:/Users/kevin/.local/bin:/Users/kevin/Library/Application Support/JetBrains/Toolbox/scripts"
